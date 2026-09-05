f03c96328dc41a198bf000df04df61fde9533a37
Deniskaaaz <www.petuk-denis@yandex.ru>
2026-09-05 17:12:45 +0300

    Use DATABASE_URL environment variable for SQLite path

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
-﻿from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+﻿import os
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
-DATABASE_URL = "sqlite+aiosqlite:///./planner.db"
+# Используем переменную окружения DATABASE_URL, если она задана
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite+aiosqlite:///./planner.db")
 
 engine = create_async_engine(DATABASE_URL, echo=False)
 AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
